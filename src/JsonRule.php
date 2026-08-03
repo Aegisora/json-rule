ccbc3fd81a811e1bb58c47d5a5908c41abaed290
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-03 17:23:54 +0400

    [AEGISORA-68250228] Implemented executeValidate.

--- src/JsonRule.php
+++ src/JsonRule.php
@@ -3,12 +3,28 @@
 namespace Aegisora\Rules;
 
 use Aegisora\RuleContract\Exceptions\InvalidRuleContextException;
+use Aegisora\RuleContract\Models\Context;
+use Aegisora\RuleContract\Models\Result;
+use Aegisora\RuleContract\Rule;
 
-class JsonRule
+class JsonRule extends Rule
 {
     public static function create(): self
     {
         return new self();
+    }
+
+    protected function executeValidate(Context $context): Result
+    {
+        $value = $context->getValue();
+
+        $this->validateValue($value);
+
+        json_decode($value);
+
+        return (json_last_error() === JSON_ERROR_NONE) ?
+            $this->getDefaultValidResult() :
+            $this->getDefaultInvalidResult();
     }
 
     /**
