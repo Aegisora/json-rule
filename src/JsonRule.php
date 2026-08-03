2647119efe631c928c1ff477613fcbb03ad7c138
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-03 17:21:10 +0400

    [AEGISORA-68250228] Implemented validateValue.

--- src/JsonRule.php
+++ src/JsonRule.php
@@ -2,10 +2,23 @@
 
 namespace Aegisora\Rules;
 
+use Aegisora\RuleContract\Exceptions\InvalidRuleContextException;
+
 class JsonRule
 {
     public static function create(): self
     {
         return new self();
+    }
+
+    /**
+     * @param mixed $value
+     * @throws InvalidRuleContextException
+     */
+    private function validateValue($value): void
+    {
+        if (!is_string($value)) {
+            throw new InvalidRuleContextException();
+        }
     }
 }
