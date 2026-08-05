d776a0831ef38ffcae09b7c955c54dc1a0a5c7ca
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-05 20:55:18 +0400

    [AEGISORA-68250228] Covered: context value - empty object.

--- tests/Unit/JsonRuleTest.php
+++ tests/Unit/JsonRuleTest.php
@@ -2,14 +2,59 @@
 
 namespace Aegisora\Rules\Tests\Unit;
 
+use Aegisora\RuleContract\Models\Context;
+use Aegisora\RuleContract\Models\Result;
 use Aegisora\RuleContract\RuleInterface;
 use Aegisora\Rules\JsonRule;
 use PHPUnit\Framework\TestCase;
 
 class JsonRuleTest extends TestCase
 {
+    private JsonRule $jsonRule;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->jsonRule = new JsonRule();
+    }
+
     public function testCreate(): void
     {
         self::assertInstanceOf(RuleInterface::class, JsonRule::create());
+    }
+
+    /**
+     * @dataProvider getValidateProvidedData
+     */
+    public function testValidate(
+        Context $context,
+        array $expectedResult
+    ): void {
+        self::assertActualResultEqualsExpected(
+            JsonRule::create()->validate($context),
+            $expectedResult
+        );
+    }
+
+    public static function getValidateProvidedData(): array
+    {
+        return [
+            'context value - empty object' => [
+                'context' => Context::create('{}'),
+                'expectedResult' => [
+                    'isValid' => true,
+                    'failedRuleCode' => null,
+                ],
+            ],
+        ];
+    }
+
+    private static function assertActualResultEqualsExpected(
+        Result $result,
+        array $expectedResult
+    ): void {
+        self::assertEquals($expectedResult['isValid'], $result->isValid());
+        self::assertEquals($expectedResult['failedRuleCode'], $result->getFailedRuleCode());
     }
 }
