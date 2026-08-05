8377dcc1f2fbe650366c6e4bf40ab86c9ab96d4e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-05 20:50:29 +0400

    [AEGISORA-68250228] Covered: Create.

--- tests/Unit/JsonRuleTest.php
+++ tests/Unit/JsonRuleTest.php
@@ -2,8 +2,14 @@
 
 namespace Aegisora\Rules\Tests\Unit;
 
+use Aegisora\RuleContract\RuleInterface;
+use Aegisora\Rules\JsonRule;
 use PHPUnit\Framework\TestCase;
 
 class JsonRuleTest extends TestCase
 {
+    public function testCreate(): void
+    {
+        self::assertInstanceOf(RuleInterface::class, JsonRule::create());
+    }
 }
